0598885fdc887203cf5d63056102c05060362d4c
Dmitry Osipa <dima.osipa@gmail.com>
2015-10-27 03:38:48 +0200

    added Pixpie-iOS Skeleton

---
+++ Example/Pods/Target Support Files/Pixpie-iOS/Pixpie-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pixpie_iOS : NSObject
+@end
+@implementation PodsDummy_Pixpie_iOS
+@end
---
+++ Example/Pods/Target Support Files/Pixpie-iOS/Pixpie-iOS-umbrella.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+#import "NSObject+SafeKVC.h"
+#import "NSString+PXPSecurity.h"
+#import "PXP.h"
+#import "PXPAccountInfo.h"
+#import "PXPAuthManager.h"
+#import "PXPAuthPrincipal.h"
+#import "PXPRequestWrapper.h"
+
+FOUNDATION_EXPORT double Pixpie_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pixpie_iOSVersionString[];
+
