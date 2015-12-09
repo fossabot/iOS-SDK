b0f13f5dab3b2148be24f3f6c0ff085f06f328c5
Dmitry Osipa <dima.osipa@gmail.com>
2015-12-09 11:35:08 +0200

    added extension for UIImageView to support PXPtransform

---
+++ Pod/Classes/UIImageView+PXPExtensions.h
@@ -0,0 +1,17 @@
+//
+//  UIImageView+PXPExtensions.h
+//  Pods
+//
+//  Created by Dmitry Osipa on 12/9/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class PXPTransform;
+
+@interface UIImageView (PXPExtensions)
+
+@property (nonatomic, strong) PXPTransform* transfrom;
+
+@end
---
+++ Pod/Classes/UIImageView+PXPExtensions.m
@@ -0,0 +1,33 @@
+//
+//  UIImageView+PXPExtensions.m
+//  Pods
+//
+//  Created by Dmitry Osipa on 12/9/15.
+//
+//
+
+#import "UIImageView+PXPExtensions.h"
+#import "PXPTransform.h"
+#import <objc/runtime.h>
+
+@implementation UIImageView (PXPExtensions)
+
+@dynamic transfrom;
+
+- (void)setTransfrom:(PXPTransform *)transform {
+    NSString* key = NSStringFromSelector(@selector(transfrom));
+    objc_setAssociatedObject(self, (__bridge const void *)(key), transform, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (PXPTransform*)transfrom {
+    NSString* key = NSStringFromSelector(@selector(transfrom));
+    PXPTransform* transform = objc_getAssociatedObject(self, (__bridge const void *)(key));
+    if (transform == nil) {
+        transform = [PXPTransform new];
+        transform.fitSize = self.bounds.size;
+        objc_setAssociatedObject(self, (__bridge const void *)(key), transform, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return transform;
+}
+
+@end
