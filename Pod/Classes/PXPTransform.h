84da92a31110c2d63afe64b0ad9d6f34b8cf65d1
Dmitry Osipa <dima.osipa@gmail.com>
2015-12-09 11:34:17 +0200

    added PXPTransfrom class

---
+++ Pod/Classes/PXPTransform.h
@@ -0,0 +1,26 @@
+//
+//  PXPTransform.h
+//  Pods
+//
+//  Created by Dmitry Osipa on 12/9/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+@import UIKit;
+
+typedef enum : NSInteger {
+    PXPTransformQualityAutomatic
+} PXPTransformQuality;
+
+typedef enum : NSInteger {
+    PXPTransformFormatAutomatic
+} PXPTransformFormat;
+
+@interface PXPTransform : NSObject
+
+@property (nonatomic, assign) PXPTransformQuality imageQuality;
+@property (nonatomic, assign) PXPTransformFormat imageFormat;
+@property (nonatomic, assign) CGSize fitSize;
+
+@end
---
+++ Pod/Classes/PXPTransform.m
@@ -0,0 +1,14 @@
+//
+//  PXPTransform.m
+//  Pods
+//
+//  Created by Dmitry Osipa on 12/9/15.
+//
+//
+
+#import "PXPTransform.h"
+
+@implementation PXPTransform
+
+
+@end
