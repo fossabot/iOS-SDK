644de4d4b793d06c1e71d349a17c7b4ca6a20727
Dmitry Osipa <dima.osipa@gmail.com>
2016-11-14 15:05:40 +0200

    removed hockeyapp from dependencies

--- Example/Pixpie-iOS/Classes/AppDelegate.swift
+++ Example/Pixpie-iOS/Classes/AppDelegate.swift
@@ -7,10 +7,6 @@
 //
 
 import UIKit
-import HockeySDK
-
-//import Fabric
-//import Crashlytics
 
 @UIApplicationMain
 
@@ -19,12 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-        BITHockeyManager.sharedHockeyManager().configureWithIdentifier("1281ec03743d493bb76953318bb49bf4")
-        // Do some additional configuration if needed here
-        BITHockeyManager.sharedHockeyManager().startManager()
-        BITHockeyManager.sharedHockeyManager().authenticator.authenticateInstallation()
 
-        //Fabric.with([Crashlytics.self])
         let defaultLicense = "41bc38fde0dfed6917b6f54fdc32761ac2d9e7eb6cd66b8591e586e6fc6b9063"
         NSUserDefaults.standardUserDefaults().registerDefaults(["pxp_license" : defaultLicense])
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(AppDelegate.splashCompleted(_:)), name: kSplashCompleteNotification, object: nil)
